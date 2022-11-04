4fa9bda79ee794c83e81e62532c2d71bb994042e
yjoonjang <yjoonjang34@gmail.com>
2022-11-04 15:29:29 +0900

    [Redux 2.3] Delete Todo

--- src/index.js
+++ src/index.js
@@ -20,11 +20,51 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+store.subscribe(() => console.log(store.getState()));
+
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deletedTodo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
+const dispatchAddTodo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deletedTodo(id));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = '';
+  toDos.forEacth((toDo) => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.addEventListener('click', dispatchDeleteToDo);
+    btn.innerText = 'DEL';
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+  });
+};
+
+store.subscribe(paintToDos);
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddTodo(toDo);
 };
 
 form.addEventListener('submit', onSubmit);
