70bca28e44f4a1be34c523cac6d27e591794a8a0
yjoonjang <yjoonjang34@gmail.com>
2022-11-04 14:58:57 +0900

    [Redux 1.4] Recap

--- src/index.js
+++ src/index.js
@@ -4,34 +4,29 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const countStore = createStore();
+number.innerText = 0;
+
+const ADD = 'ADD';
+const MINUS = 'MINUS';
 
 const countModifier = (count = 0, action) => {
-  if (action.type === 'ADD') {
-    return count + 1;
-  } else if (action.type === 'MINUS') {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
-// let count = 0;
-// number.innerText = count;
+const countStore = createStore(countModifier);
 
-// const updateText = () => {
-//   number.innerText = count;
-// };
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-// const handleAdd = () => {
-//   count++;
-//   updateText();
-// };
+countStore.subscribe(onChange);
 
-// const handleMinus = () => {
-//   count--;
-//   updateText();
-// };
-
-// add.addEventListener('click', handleAdd);
-// minus.addEventListener('click', handleMinus);
+add.addEventListener('click', () => countStore.dispatch({ type: ADD }));
+minus.addEventListener('click', () => countStore.dispatch({ type: MINUS }));
