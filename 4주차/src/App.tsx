e0ecdc9ae7b99623a82b593d0b4287ec14801921
장영준 <jang-youngjoon@jang-yeongjun-ui-MacBookAir.local>
2022-11-18 16:56:01 +0900

    8.10

--- 4주차/src/App.tsx
+++ 4주차/src/App.tsx
@@ -1,71 +1,104 @@
-import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import styled from 'styled-components';
-import { toDoState } from './atoms';
-import { useRecoilState } from 'recoil';
-import Board from './Components/Board';
+import { motion, useMotionValue, useScroll, useTransform } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 
 const Wrapper = styled.div`
-  display: flex;
+  height: 100vh;
   width: 100vw;
-  margin: 0 auto;
+  display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
 `;
 
-const Boards = styled.div`
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
   display: flex;
   justify-content: center;
-  align-items: flex-start;
-  width: 100%;
-  gap: 10px;
+  align-items: center;
 `;
 
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Svg = styled.svg`
+  width: 200px;
+  height: 300px;
+`;
+
+const myVars = {
+  start: {
+    scale: 0,
+  },
+  end: { scale: 1, rotateZ: 360, transition: { type: 'spring', delay: 0.5 } },
+};
+
+const boxVariants: any = {
+  hover: {
+    scale: 1.5,
+    rotate: '90deg',
+  },
+  click: {
+    scale: 1,
+    borderRadius: '100px',
+  },
+  drag: {
+    backgroundColor: 'rgb(46, 204, 113)',
+    transition: { duration: 10 },
+  },
+};
+
+// const circleVariants = {
+//   start: {
+//     opacity: 0,
+//     y: -10,
+//   },
+//   end: {
+//     opacity: 1,
+//     y: 0,
+//   },
+// };
+
+const svg = {
+  start: { pathLength: 0, fill: 'rgba(255, 255, 255, 0)' },
+  end: {
+    pathLength: 1,
+    fill: 'rgba(255, 255, 255, 1)',
+    transition: { duration: 4 },
+  },
+};
+
 function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = (info: DropResult) => {
-    console.log(info);
-    const {destination, source} = info;
-    if (!destination) return;
-    if(destination?.droppableId === source.droppableId){
-      //같은 보드 내 이동
-      setToDos((allBoards) => {
-        const boardCopy = [...allBoards[source.droppableId]];
-        const taskObj = boardCopy[source.index];
-        boardCopy.splice(source.index, 1); //한개 지우기
-        boardCopy.splice(destination?.index, 0, taskObj); //draggableId 추가하기
-        return {
-          ...allBoards,
-          [source.droppableId]: boardCopy
-        };
-    });
-    }
-    if (destination.droppableId !== source.droppableId) {
-      // 다른 보드로 이동
-      setToDos((allBoards) => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const taskObj = sourceBoard[source.index];
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
-  };
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Wrapper>
-        <Boards>
-          {Object.keys(toDos).map((boardId) => (
-            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
-          ))}
-        </Boards>
-      </Wrapper>
-    </DragDropContext>
+    <Wrapper>
+      <Svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <motion.path
+            variants={svg}
+            initial="start"
+            animate="end"
+            stroke="white"
+            strokeWidth="2"
+            d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
+          />
+        </svg>
+      </Svg>
+    </Wrapper>
   );
 }
 
